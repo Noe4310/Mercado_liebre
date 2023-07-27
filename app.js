0a073bfb84af49fe3600d5911a5451821a4183f5
Noe4310 <noli_ramos@hotmail.com>
2023-07-27 16:36:28 -0300

    Proyecto individual

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const PORT = 3030;
+
+app.use(express.json());
+app.use("/", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/views/register.html");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/views/login.html");
+});
+
+app.listen(PORT, () => {
+  console.log("Aplicación esuchando en puerto 3030");
+});
